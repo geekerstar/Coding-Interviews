e12d9f07281c89501981d195cbe0973856ff2e96
geekerstar <247507792@qq.com>
2018-08-29 18:10:47 +0800

    finish s11

---
+++ 11-Number-Of-1/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * @Author: Geekerstar(QQ : 247507792)
+ * @Date: 2018/8/29 18:04
+ * @Description:
+ */
+public class Solution {
+    public int NumberOf1(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            cnt++;
+            n &= (n - 1);
+        }
+        return cnt;
+    }
+}
