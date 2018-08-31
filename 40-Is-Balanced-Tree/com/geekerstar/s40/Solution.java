0fa011622887433a1f9fbd3186673a7feaa450ef
geekerstar <247507792@qq.com>
2018-08-31 18:27:10 +0800

    finish s40

---
+++ 40-Is-Balanced-Tree/com/geekerstar/s40/Solution.java
@@ -0,0 +1,35 @@
+package com.geekerstar.s40;
+
+/**
+ * @Author: Geekerstar(QQ : 247507792)
+ * @Date: 2018/8/31 18:23
+ * @Description:
+ */
+public class Solution {
+    private boolean isBalanced = true;
+
+    public boolean IsBalanced_Solution(TreeNode root) {
+        height(root);
+        return isBalanced;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null || !isBalanced)
+            return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if (Math.abs(left - right) > 1)
+            isBalanced = false;
+        return 1 + Math.max(left, right);
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
