0b0216631c4c19113141332e16e39b3dac5e7385
geekerstar <247507792@qq.com>
2018-10-09 13:39:30 +0800

    add solution to s18

--- 18-Mirror/com/geekerstar/s18/Solution.java
+++ 18-Mirror/com/geekerstar/s18/Solution.java
@@ -7,8 +7,9 @@ package com.geekerstar.s18;
  */
 public class Solution {
     public void Mirror(TreeNode root) {
-        if (root == null)
+        if (root == null) {
             return;
+        }
         swap(root);
         Mirror(root.left);
         Mirror(root.right);
@@ -24,10 +25,8 @@ public class Solution {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
         public TreeNode(int val) {
             this.val = val;
-
         }
     }
 }
