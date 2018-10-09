6fd28fa5740eb0258828183d4dc462cd44304c95
geekerstar <247507792@qq.com>
2018-10-09 13:13:39 +0800

    add solution to s17

--- 17-Has-Subtree/com/geekerstar/s17/Solution.java
+++ 17-Has-Subtree/com/geekerstar/s17/Solution.java
@@ -7,18 +7,22 @@ package com.geekerstar.s17;
  */
 public class Solution {
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null)
+        if (root1 == null || root2 == null) {
             return false;
+        }
         return isSubtreeWithRoot(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
     }
 
     private boolean isSubtreeWithRoot(TreeNode root1, TreeNode root2) {
-        if (root2 == null)
+        if (root2 == null) {
             return true;
-        if (root1 == null)
+        }
+        if (root1 == null) {
             return false;
-        if (root1.val != root2.val)
+        }
+        if (root1.val != root2.val) {
             return false;
+        }
         return isSubtreeWithRoot(root1.left, root2.left) && isSubtreeWithRoot(root1.right, root2.right);
     }
 
@@ -26,7 +30,6 @@ public class Solution {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
         public TreeNode(int val) {
             this.val = val;
 
