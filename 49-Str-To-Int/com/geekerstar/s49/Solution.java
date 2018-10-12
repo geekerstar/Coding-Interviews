e2ba326ad4d23f44fa82dd438931bc15d007b06d
geekerstar <247507792@qq.com>
2018-10-12 09:28:18 +0800

    update md

--- 49-Str-To-Int/com/geekerstar/s49/Solution.java
+++ 49-Str-To-Int/com/geekerstar/s49/Solution.java
@@ -13,10 +13,14 @@ public class Solution {
         int ret = 0;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (i == 0 && (c == '+' || c == '-'))  /* 符号判定 */
+            /* 符号判定 */
+            if (i == 0 && (c == '+' || c == '-')) {
                 continue;
-            if (c < '0' || c > '9')                /* 非法输入 */
+            }
+            /* 非法输入 */
+            if (c < '0' || c > '9') {
                 return 0;
+            }
             ret = ret * 10 + (c - '0');
         }
         return isNegative ? -ret : ret;
