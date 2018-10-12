eea7f908c9fdf5e57a831479c978887207308b35
geekerstar <247507792@qq.com>
2018-10-12 09:11:13 +0800

    update md

--- 46-Last-Remaining/com/geekerstar/s46/Solution.java
+++ 46-Last-Remaining/com/geekerstar/s46/Solution.java
@@ -7,10 +7,14 @@ package com.geekerstar.s46;
  */
 public class Solution {
     public int LastRemaining_Solution(int n, int m) {
-        if (n == 0)     /* 特殊输入的处理 */
+        /* 特殊输入的处理 */
+        if (n == 0) {
             return -1;
-        if (n == 1)     /* 递归返回条件 */
+        }
+        /* 递归返回条件 */
+        if (n == 1) {
             return 0;
+        }
         return (LastRemaining_Solution(n - 1, m) + m) % n;
     }
 }
