824c6d3971aeff3ecb473243925c41c23865eeac
geekerstar <247507792@qq.com>
2018-10-12 09:46:52 +0800

    update md

--- 52-Reg-Match/com/geekerstar/s52/Solution.java
+++ 52-Reg-Match/com/geekerstar/s52/Solution.java
@@ -12,22 +12,32 @@ public class Solution {
         boolean[][] dp = new boolean[m + 1][n + 1];
 
         dp[0][0] = true;
-        for (int i = 1; i <= n; i++)
-            if (pattern[i - 1] == '*')
+        for (int i = 1; i <= n; i++) {
+            if (pattern[i - 1] == '*') {
                 dp[0][i] = dp[0][i - 2];
+            }
+        }
 
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= n; j++)
-                if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.')
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
-                else if (pattern[j - 1] == '*')
+                }
+                else if (pattern[j - 1] == '*') {
                     if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.') {
-                        dp[i][j] |= dp[i][j - 1]; // a* counts as single a
-                        dp[i][j] |= dp[i - 1][j]; // a* counts as multiple a
-                        dp[i][j] |= dp[i][j - 2]; // a* counts as empty
-                    } else
-                        dp[i][j] = dp[i][j - 2];   // a* only counts as empty
-
+                        // a* counts as single a
+                        dp[i][j] |= dp[i][j - 1];
+                        // a* counts as multiple a
+                        dp[i][j] |= dp[i - 1][j];
+                        // a* counts as empty
+                        dp[i][j] |= dp[i][j - 2];
+                    } else {
+                        // a* only counts as empty
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
         return dp[m][n];
     }
 }
