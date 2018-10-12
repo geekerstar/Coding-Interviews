25c20449add042f3f7bb0bb37189731f63c008d1
geekerstar <247507792@qq.com>
2018-10-12 10:28:57 +0800

    update md

--- 57-Delete-Duplication/com/geekerstar/s57/Solution.java
+++ 57-Delete-Duplication/com/geekerstar/s57/Solution.java
@@ -7,12 +7,14 @@ package com.geekerstar.s57;
  */
 public class Solution {
     public ListNode deleteDuplication(ListNode pHead) {
-        if (pHead == null || pHead.next == null)
+        if (pHead == null || pHead.next == null) {
             return pHead;
+        }
         ListNode next = pHead.next;
         if (pHead.val == next.val) {
-            while (next != null && pHead.val == next.val)
+            while (next != null && pHead.val == next.val) {
                 next = next.next;
+            }
             return deleteDuplication(next);
         } else {
             pHead.next = deleteDuplication(pHead.next);
@@ -23,7 +25,6 @@ public class Solution {
     public class ListNode {
         int val;
         ListNode next = null;
-
         ListNode(int val) {
             this.val = val;
         }
