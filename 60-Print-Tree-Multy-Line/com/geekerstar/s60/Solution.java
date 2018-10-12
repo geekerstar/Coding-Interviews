dc26991e9e81e83b59ea79f51fef31c61e6712ac
geekerstar <247507792@qq.com>
2018-10-12 10:38:42 +0800

    update md

--- 60-Print-Tree-Multy-Line/com/geekerstar/s60/Solution.java
+++ 60-Print-Tree-Multy-Line/com/geekerstar/s60/Solution.java
@@ -19,14 +19,16 @@ public class Solution {
             int cnt = queue.size();
             while (cnt-- > 0) {
                 TreeNode node = queue.poll();
-                if (node == null)
+                if (node == null) {
                     continue;
+                }
                 list.add(node.val);
                 queue.add(node.left);
                 queue.add(node.right);
             }
-            if (list.size() != 0)
+            if (list.size() != 0) {
                 ret.add(list);
+            }
         }
         return ret;
     }
@@ -35,7 +37,6 @@ public class Solution {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
         public TreeNode(int val) {
             this.val = val;
         }
