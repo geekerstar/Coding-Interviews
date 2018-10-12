73a75fb8c1ca0adfbab9e4768e1586dacc40dc3c
geekerstar <247507792@qq.com>
2018-10-12 09:42:41 +0800

    update md

--- 51-Multiply/com/geekerstar/s51/Solution.java
+++ 51-Multiply/com/geekerstar/s51/Solution.java
@@ -9,10 +9,14 @@ public class Solution {
     public int[] multiply(int[] A) {
         int n = A.length;
         int[] B = new int[n];
-        for (int i = 0, product = 1; i < n; product *= A[i], i++)       /* 从左往右累乘 */
+        /* 从左往右累乘 */
+        for (int i = 0, product = 1; i < n; product *= A[i], i++) {
             B[i] = product;
-        for (int i = n - 1, product = 1; i >= 0; product *= A[i], i--)  /* 从右往左累乘 */
+        }
+        /* 从右往左累乘 */
+        for (int i = n - 1, product = 1; i >= 0; product *= A[i], i--) {
             B[i] *= product;
+        }
         return B;
     }
 }
