56698e6fb4ceabd91a36882d5155e5d86dbf9559
geekerstar <247507792@qq.com>
2018-10-12 08:56:02 +0800

    update md

--- 45-Is-Continuous/com/geekerstar/s45/Solution.java
+++ 45-Is-Continuous/com/geekerstar/s45/Solution.java
@@ -10,21 +10,25 @@ import java.util.Arrays;
 public class Solution {
     public boolean isContinuous(int[] nums) {
 
-        if (nums.length < 5)
+        if (nums.length < 5) {
             return false;
+        }
 
         Arrays.sort(nums);
 
         // 统计癞子数量
         int cnt = 0;
-        for (int num : nums)
-            if (num == 0)
+        for (int num : nums) {
+            if (num == 0) {
                 cnt++;
+            }
+        }
 
         // 使用癞子去补全不连续的顺子
         for (int i = cnt; i < nums.length - 1; i++) {
-            if (nums[i + 1] == nums[i])
+            if (nums[i + 1] == nums[i]) {
                 return false;
+            }
             cnt -= nums[i + 1] - nums[i] - 1;
         }
         return cnt >= 0;
