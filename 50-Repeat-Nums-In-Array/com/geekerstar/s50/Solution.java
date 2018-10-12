9bafedec9554a2f9f42524495439f6c08aa7d49e
geekerstar <247507792@qq.com>
2018-10-12 09:32:58 +0800

    update md

--- 50-Repeat-Nums-In-Array/com/geekerstar/s50/Solution.java
+++ 50-Repeat-Nums-In-Array/com/geekerstar/s50/Solution.java
@@ -5,10 +5,21 @@ package com.geekerstar.s50;
  * @Date: 2018/8/31 19:31
  * @Description:
  */
+
+// Parameters:
+//    numbers:     an array of integers
+//    length:      the length of array numbers
+//    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+//                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+//    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+// Return value:       true if the input is valid, and there are some duplications in the array number
+//                     otherwise false
+
 public class Solution {
     public boolean duplicate(int[] nums, int length, int[] duplication) {
-        if (nums == null || length <= 0)
+        if (nums == null || length <= 0) {
             return false;
+        }
         for (int i = 0; i < length; i++) {
             while (nums[i] != i) {
                 if (nums[i] == nums[nums[i]]) {
