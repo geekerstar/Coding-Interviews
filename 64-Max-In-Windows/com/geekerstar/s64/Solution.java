5122c0dbc318a82792e32498e2896708bcf85d9c
geekerstar <247507792@qq.com>
2018-10-12 10:47:58 +0800

    update md

--- 64-Max-In-Windows/com/geekerstar/s64/Solution.java
+++ 64-Max-In-Windows/com/geekerstar/s64/Solution.java
@@ -11,13 +11,17 @@ import java.util.PriorityQueue;
 public class Solution {
     public ArrayList<Integer> maxInWindows(int[] num, int size) {
         ArrayList<Integer> ret = new ArrayList<>();
-        if (size > num.length || size < 1)
+        if (size > num.length || size < 1) {
             return ret;
-        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> o2 - o1);  /* 大顶堆 */
-        for (int i = 0; i < size; i++)
+        }
+        /* 大顶堆 */
+        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int i = 0; i < size; i++) {
             heap.add(num[i]);
+        }
         ret.add(heap.peek());
-        for (int i = 1, j = i + size - 1; j < num.length; i++, j++) {            /* 维护一个大小为 size 的大顶堆 */
+        /* 维护一个大小为 size 的大顶堆 */
+        for (int i = 1, j = i + size - 1; j < num.length; i++, j++) {
             heap.remove(num[i - 1]);
             heap.add(num[j]);
             ret.add(heap.peek());
