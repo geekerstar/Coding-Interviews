92ab14446d1e1723da9e2c12fb866fc9c8205f8f
geekerstar <247507792@qq.com>
2018-10-12 10:31:42 +0800

    update md

--- 58-Is-Symmetrical/com/geekerstar/s58/Solution.java
+++ 58-Is-Symmetrical/com/geekerstar/s58/Solution.java
@@ -7,18 +7,22 @@ package com.geekerstar.s58;
  */
 public class Solution {
     boolean isSymmetrical(TreeNode pRoot) {
-        if (pRoot == null)
+        if (pRoot == null) {
             return true;
+        }
         return isSymmetrical(pRoot.left, pRoot.right);
     }
 
     boolean isSymmetrical(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null)
+        if (t1 == null && t2 == null) {
             return true;
-        if (t1 == null || t2 == null)
+        }
+        if (t1 == null || t2 == null) {
             return false;
-        if (t1.val != t2.val)
+        }
+        if (t1.val != t2.val) {
             return false;
+        }
         return isSymmetrical(t1.left, t2.right) && isSymmetrical(t1.right, t2.left);
     }
 
@@ -26,7 +30,6 @@ public class Solution {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
         public TreeNode(int val) {
             this.val = val;
 
